c0e807c8528c2087382bdddf219d67aff30faec3
Xiduo <iris5202@gmail.com>
2020-09-18 05:58:33 +0800

    add for loop

--- products.py
+++ products.py
@@ -6,3 +6,10 @@ while True:
     price = input('請輸入商品價格: ')
     products.append([name, price])  #建立二維清單中的小清單
 print(products)
+
+for p in products:
+    print(p)
+for p in products:
+    print(p[0]) #印出每個小清單的第0項
+for p in products:
+    print(p[0], '的價格是', p[1])
